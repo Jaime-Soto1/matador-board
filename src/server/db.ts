725758180bbcfa127eb09047375d98e8aa113582
jaime soto <81286832+Jaime-Soto1@users.noreply.github.com>
2026-06-13 21:37:15 -0700

    Use environment variables for database config

--- src/server/db.ts
+++ src/server/db.ts
@@ -1,10 +1,13 @@
 import mysql from 'mysql2';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: 'Lakers0318',
-  database: 'matadorboard',
-})
+  host: process.env.DB_HOST || 'localhost',
+  user: process.env.DB_USER || 'root',
+  password: process.env.DB_PASSWORD || '',
+  database: process.env.DB_NAME || 'matadorboard',
+});
 
 export default pool;
